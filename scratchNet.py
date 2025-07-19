5bab05c4d0b199d69f9fa7ceaf799c14a5f0d461
eikep <197432614+ep2212@users.noreply.github.com>
2025-07-19 19:08:33 +0200

    Environment 3.12 aufgesetzt und nötige packages

---
+++ scratchNet.py
@@ -0,0 +1,8 @@
+import abc
+import matplotlib.pyplot as plt
+import tensorflow as tf
+import numpy as np
+import random
+import time
+from tqdm import tqdm, trange
+from deeplearning2020 import helpers
